93c56b5866831bf42d6403a5c8e8e59457a161c9
Manan <mpachich@in.ibm.com>
2018-10-04 11:27:25 +0530

    1

--- kubernetes.tf
+++ kubernetes.tf
@@ -44,10 +44,10 @@ resource "kubernetes_pod" "jenkins" {
       image = "jenkins/jenkins:latest"
       name  = "example"
 
-      # env = {
-      #   name  = "CASC_JENKINS_CONFIG"
-      #   value = "https://github.com/mpachich/Kubernetes_template/blob/master/config.yaml"
-      # }
+      env = {
+        name  = "CASC_JENKINS_CONFIG"
+        value = "https://github.com/mpachich/Kubernetes_template/blob/master/config.yaml"
+      }
 
       port {
         container_port = 80
